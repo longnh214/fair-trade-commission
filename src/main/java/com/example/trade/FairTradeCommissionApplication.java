b78723d969466d6365c4a7c28eded2abe56c33ab
Nakhoon <cnh6123@naver.com>
2025-04-05 21:34:53 +0900

    [INIT] project init

---
+++ src/main/java/com/example/trade/FairTradeCommissionApplication.java
@@ -0,0 +1,13 @@
+package com.example.trade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FairTradeCommissionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FairTradeCommissionApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/trade/service/CsvService.java
@@ -0,0 +1,10 @@
+package com.example.trade.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CsvService {
+    public void download(String city, String district){
+
+    }
+}
