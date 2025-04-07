8c178d6b32c9a6bd2b05c5b253fb7fc7bcfdf006
Nakhoon <cnh6123@naver.com>
2025-04-07 16:50:42 +0900

    [ADD] Thread pool config 작성

--- src/main/java/com/example/trade/config/ThreadPoolConfig.java
+++ src/main/java/com/example/trade/config/ThreadPoolConfig.java
@@ -1,2 +1,23 @@
-package com.example.trade.config;public class ThreadPoolConfig {
+package com.example.trade.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+public class ThreadPoolConfig implements AsyncConfigurer {
+    @Bean(name = "taskExecutor")
+    public Executor taskExecutor() {
+        int core = Runtime.getRuntime().availableProcessors();
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(core);
+        executor.setMaxPoolSize(core * 2);
+        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("Async-");
+        executor.initialize();
+        return executor;
+    }
 }
