22103cfb35cd101c7c9a58369bb2e6d0fe9205ee
Nakhoon <cnh6123@naver.com>
2025-04-08 20:00:51 +0900

    [ADD] openapi (Swagger) 설정 추가

---
+++ src/main/java/com/example/trade/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package com.example.trade.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("법인 데이터 관리 시스템")
+                .description("법인 데이터 관리 시스템")
+                .version("1.0.0");
+    }
+}
