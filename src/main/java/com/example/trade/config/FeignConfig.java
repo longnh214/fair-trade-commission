0f7e1f75c508d5984f63c163229f2b8eeb925c07
Nakhoon <cnh6123@naver.com>
2025-04-09 16:15:08 +0900

    [UPDATE] open-feign 설정에 재시도 로직 추가

--- src/main/java/com/example/trade/config/FeignConfig.java
+++ src/main/java/com/example/trade/config/FeignConfig.java
@@ -1,15 +1,24 @@
 package com.example.trade.config;
 
 import feign.RequestInterceptor;
+import feign.Retryer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
+
 @Configuration
 public class FeignConfig {
     @Bean
     public RequestInterceptor requestInterceptor() {
         return requestTemplate -> requestTemplate
-                .header("Accept", "*/*")
+                .header("Accept", "application/json, text/csv")
                 .header("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Whale/4.30.291.11 Safari/537.36")
                 .header("Accept-Encoding", "gzip, deflate, br");
+    }
+
+    @Bean
+    public Retryer.Default retryer() {
+        return new Retryer.Default(1000L, TimeUnit.SECONDS.toMillis(3L), 5);
     }
 }
