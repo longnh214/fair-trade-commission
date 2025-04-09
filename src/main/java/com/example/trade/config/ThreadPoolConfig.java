8e619f4f14c3aa30ec37baaa5ac50999eccba71d
Nakhoon <cnh6123@naver.com>
2025-04-09 16:38:04 +0900

    [CHORE] 주석 제거 및 IMPORT 정리

--- src/main/java/com/example/trade/config/ThreadPoolConfig.java
+++ src/main/java/com/example/trade/config/ThreadPoolConfig.java
@@ -3,9 +3,11 @@ package com.example.trade.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
-import java.util.concurrent.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class ThreadPoolConfig implements AsyncConfigurer {
@@ -13,8 +15,8 @@ public class ThreadPoolConfig implements AsyncConfigurer {
     public ExecutorService customExecutor() {
         int coreCount = Runtime.getRuntime().availableProcessors();
         return new ThreadPoolExecutor(
-                coreCount * 2, // corePoolSize
-                coreCount * 4, // maximumPoolSize
+                coreCount * 2,
+                coreCount * 4,
                 60L, TimeUnit.SECONDS,
                 new LinkedBlockingQueue<>(10000000), // 큐 크기: 넉넉하게
                 new ThreadPoolExecutor.CallerRunsPolicy() // 거부 시 현재 스레드가 실행
