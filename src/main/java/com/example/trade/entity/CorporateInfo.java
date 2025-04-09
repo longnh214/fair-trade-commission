31cb431602e21c9b8bdacc6cf81a13335923554c
Nakhoon <cnh6123@naver.com>
2025-04-09 16:23:15 +0900

    [ADD] DB에 저장할 법인 정보 관련 entity와 repository 작성

--- src/main/java/com/example/trade/entity/CorporateInfo.java
+++ src/main/java/com/example/trade/entity/CorporateInfo.java
@@ -1,2 +1,37 @@
-package com.example.trade.entity;public class CorporateInfo {
+package com.example.trade.entity;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "corporate_info")
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CorporateInfo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Schema(name = "통신판매번호")
+    private String commSaleNumber;
+
+    @Schema(name = "상호")
+    private String businessName;
+
+    @Schema(name = "사업자등록번호")
+    private String businessNumber;
+
+    @Schema(name = "법인등록번호")
+    private String corporateNumber;
+
+    @Schema(name = "행정구역코드")
+    private String districtCode;
 }
--- src/main/java/com/example/trade/repository/CorporateInfoRepository.java
+++ src/main/java/com/example/trade/repository/CorporateInfoRepository.java
@@ -1,2 +1,9 @@
-package com.example.trade.repository;public interface CorporateInfoRepository {
+package com.example.trade.repository;
+
+import com.example.trade.entity.CorporateInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CorporateInfoRepository extends JpaRepository<CorporateInfo, Long> {
 }
