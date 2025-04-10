4722728f857d955410f4a0dc49ad38c30f83784c
Nakhoon <cnh6123@naver.com>
2025-04-10 13:35:45 +0900

    [ADD] 통신판매사업자 파일 파싱 XLS, CSV 분기처리 service 추가

--- src/main/java/com/example/trade/service/FtcDataParsingService.java
+++ src/main/java/com/example/trade/service/FtcDataParsingService.java
@@ -1,2 +1,25 @@
-package com.example.trade.service;public class FtcDataParsingService {
+package com.example.trade.service;
+
+import com.example.trade.constant.City;
+import com.example.trade.dto.FtcDataDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.io.InputStream;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class FtcDataParsingService {
+
+    private final CsvParsingService csvParsingService;
+    private final XlsParsingService xlsParsingService;
+
+    public List<FtcDataDto> parseToDto(City city, InputStream inputStream) throws Exception {
+        if (city == City.FOREIGN) {
+            return xlsParsingService.parseToDto(inputStream);
+        } else {
+            return csvParsingService.parseToDto(inputStream);
+        }
+    }
 }
