1ca2d4d687a7a0902028d48a241c9a42493a3634
Nakhoon <cnh6123@naver.com>
2025-04-10 13:22:48 +0900

    [ADD] XLS 파일 파싱 service 추가

--- src/main/java/com/example/trade/service/XlsParsingService.java
+++ src/main/java/com/example/trade/service/XlsParsingService.java
@@ -1,2 +1,54 @@
-package com.example.trade.service;public class XlsParsingService {
+package com.example.trade.service;
+
+import com.example.trade.dto.FtcDataDto;
+import lombok.extern.log4j.Log4j2;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@Log4j2
+public class XlsParsingService {
+    @Value("${external.corporate}")
+    private String corporate;
+
+    public List<FtcDataDto> parseToDto(InputStream xlsInputStream) throws Exception {
+        List<FtcDataDto> results = new ArrayList<>();
+        Workbook workbook = new HSSFWorkbook(xlsInputStream);
+        Sheet sheet = workbook.getSheetAt(0);
+
+        for (Row row : sheet) {
+            if (row.getRowNum() == 0) continue;
+
+            FtcDataDto dto = FtcDataDto.builder()
+                    .commSaleNumber(getCellString(row.getCell(1)))
+                    .isCorporate(convertCorporate(getCellString(row.getCell(3))))
+                    .businessName(getCellString(row.getCell(4)))
+                    .businessNumber(getCellString(row.getCell(5)))
+                    .address(getCellString(row.getCell(6)))
+                    .build();
+
+            results.add(dto);
+        }
+
+        return results;
+    }
+
+    private String convertCorporate(String isCorporate){
+        if(isCorporate.equals("Y")){
+            return corporate;
+        }
+        return isCorporate;
+    }
+
+    private String getCellString(Cell cell) {
+        if (cell == null) return "";
+        DataFormatter formatter = new DataFormatter();
+        return formatter.formatCellValue(cell);
+    }
 }
