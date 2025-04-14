644358a3ed9b6019ad654b3ee6e165eb46570f70
Nakhoon <cnh6123@naver.com>
2025-04-14 15:53:39 +0900

    [UPDATE] optimize imports

--- src/test/java/com/example/trade/service/CsvParsingServiceTest.java
+++ src/test/java/com/example/trade/service/CsvParsingServiceTest.java
@@ -4,8 +4,6 @@ import com.example.trade.dto.FtcDataDto;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
